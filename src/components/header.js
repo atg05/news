6ebd9211895b6c0f603c11d4450358cdb39dc9d9
Avinash Kumar <itsmeatg@outlook.com>
2021-09-24 10:27:00 +0530

    Initial

---
+++ src/components/header.js
@@ -0,0 +1,16 @@
+import React ,{Component} from 'react';
+
+
+class  Header extends React.Component{
+    render(){
+        return (
+            <header>
+                <div>Logo</div>
+                <input></input>
+            </header>
+    
+    )}
+
+}
+
+export default Header;
