f0841f6b0fe0c9209d55382f35a0e40963244780
Avinash Kumar <itsmeatg@outlook.com>
2021-09-28 12:20:41 +0530

    Modyifying Header

--- src/components/header.js
+++ src/components/header.js
@@ -1,12 +1,19 @@
-import React from 'react';
+import React ,{useState} from 'react';
 
 
 
 const Header = (props) =>{
+
+    let [count,setCount] = useState(0);
     return(
         <header>
             <div className="logo">Logo</div>
             <input onChange={props.keywords}  />
+            <h1>Count: {count}</h1>
+            <button onClick={()=>setCount(count+1)}>Add One</button>
+            <button onClick={()=>setCount(0)}>RESET</button>
+            <button onClick={()=>setCount(count-1)}>Minus One</button>
+            
         </header>
     )
 }
